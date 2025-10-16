f92069770461aa659276684838b48e2483a270cd
dbravos <dbrasan@gmail.com>
2025-10-16 13:51:13 -0700

    sin el idelrun

--- ctrlinfo/urls.py
+++ ctrlinfo/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from idlelib import run
+# from idlelib import run
 
 from django.contrib import admin
 from django.urls import path
